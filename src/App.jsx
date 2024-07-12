d73824afe7061f4e4f207d9edde724ee8e805b50
Harsh <harshsik7890@gmail.com>
2024-07-12 12:24:52 +0530

    Created Header and added Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <div className="text-rose-800">App</div>;
-};
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+}

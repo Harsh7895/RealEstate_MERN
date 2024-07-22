a3c0bb91a3d47165331a126439f623e119b914ee
Harsh <harshsik7890@gmail.com>
2024-07-22 09:05:11 +0530

    config for deploying

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://harsh-estate-api.vercel.app",
+        changeOrigin: true,
         secure: false,
       },
     },

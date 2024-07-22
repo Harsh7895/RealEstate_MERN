7b5cba81a6b12066428eda99261d9c9b55bd959d
Harsh <harshsik7890@gmail.com>
2024-07-22 08:56:59 +0530

    uploaded backend

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://harsh-estate-api.vercel.app",
         secure: false,
       },
     },

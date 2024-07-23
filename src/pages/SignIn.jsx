0ff8a8c5d2386cc6c1a0b30358ef78998be08c53
Harsh <harshsik7890@gmail.com>
2024-07-23 15:48:27 +0530

    .

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -57,5 +57,48 @@ export default function SignIn() {
     }
   };
 
-  return <div>sign-in</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg  outline-none"
+          id="email"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg outline-none"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          value={formData.password}
+          required
+        />
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading ..." : "Sign In"}
+        </button>
+        <Oauth />
+      </form>
+
+      <div className="flex gap-2 mt-5 ">
+        <p>Dont have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+
+      {error && <p className="text-rose-700">{error}</p>}
+    </div>
+  );
 }

6dfc8024a7d6517892f64002bca5df687fac8182
Harsh <harshsik7890@gmail.com>
2024-07-24 13:10:10 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {},
+  server: {
+    proxy: {
+      // Proxy all requests starting with '/api' to the backend server
+      "/api": {
+        target: "http://localhost:3000", // Replace with your backend server URL
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react()],
 });

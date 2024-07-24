ba214d3ef1a345779c6048c35e0f1a2b1b1bdd41
Harsh <harshsik7890@gmail.com>
2024-07-24 17:07:58 +0530

    fix listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -133,18 +133,22 @@ export default function Listing() {
               </li>
             </ul>
 
-            {listing && currentUser._id !== listing.userRef && !contact && (
-              <button
-                onClick={() => setContact(true)}
-                className="bg-slate-700 text-white p-3 rounded-lg uppercase mt-8"
-              >
-                Contact Landowner
-              </button>
-            )}
+            {listing &&
+              currentUser &&
+              currentUser._id !== listing.userRef &&
+              !contact && (
+                <button
+                  onClick={() => setContact(true)}
+                  className="bg-slate-700 text-white p-3 rounded-lg uppercase mt-8"
+                >
+                  Contact Landowner
+                </button>
+              )}
 
-            {listing && currentUser._id !== listing.userRef && contact && (
-              <Contact listing={listing} />
-            )}
+            {listing &&
+              currentUser &&
+              currentUser._id !== listing.userRef &&
+              contact && <Contact listing={listing} />}
           </div>
         </>
       )}
